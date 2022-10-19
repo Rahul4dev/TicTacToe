ac5519be20aae67c0d126d29298b48ea30848849
Rahul4dev <rahulsinghsignin@gmail.com>
2022-10-19 13:16:28 +0530

    Completed TICTACTOE Game, its styling and Js, however I still need JS practice to understand the codes. I know why He used the variable and function but I don't know how I can use or derive it.

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,30 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML =
+    "You Won, <span id='winner-name'>PLAYER NAME </span>!";
+    gameOverElement.style.display = "none";
+
+    let gameBoardIndex = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        gameData[i][j] = 0;
+        const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+        gameBoardItemElement.textContent = '';
+        gameBoardItemElement.classList.remove('disabled');
+        gameBoardIndex++;
+      }
+    }
+}
+
 function startNewGame() {
   if (players[0].name === "" || players[1].name === "") {
     alert("Please set custom player names for both players!");
     return;
   }
+
+  resetGameStatus();
 
   activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = "block";
@@ -18,7 +40,7 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
-  if (event.target.tagName !== "LI") {
+  if (event.target.tagName !== "LI"  || gameIsOver) {
     return;
   }
 
@@ -35,9 +57,9 @@ function selectGameField(event) {
   selectedField.classList.add("disabled");
 
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
-  
+
   const winnerId = checkForGameOver();
-  
+
   if (winnerId !== 0) {
     endGame(winnerId);
   }
@@ -92,17 +114,15 @@ function checkForGameOver() {
       return gameData[0][i];
     }
   }
- 
 
-
-// Diagonal: top left to bottom right
-if (
-  gameData[0][0] > 0 &&
-  gameData[0][0] === gameData[1][1] &&
-  gameData[1][1] === gameData[2][2]
-) {
-  return gameData[0][0];
-}
+  // Diagonal: top left to bottom right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
 
   // Diagonal: Bottom left to top right
   if (
@@ -118,19 +138,17 @@ if (
     return -1;
   }
   return 0;
-
 }
 
 function endGame(winnerId) {
-  gameOverElement.style.display = 'block';
+  gameIsOver = true;
+  gameOverElement.style.display = "block";
 
   if (winnerId > 0) {
-    const winnerName = players[winnerId -1].name;
-    gameOverElement.firstElementChild.firstElementChild.textContent = winnerName;
-
-  } else  {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+  } else {
     gameOverElement.firstElementChild.textContent = "It's a Draw!";
   }
-
-   
 }
